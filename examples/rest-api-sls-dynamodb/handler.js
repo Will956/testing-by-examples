86bbd4f62a58ecb711b290296145b2184616aaf8
William Duplenne <will.dupl95@gmail.com>
2019-10-05 13:58:44 +0200

    :art: Improve structure

--- examples/rest-api-sls-dynamodb/handler.js
+++ examples/rest-api-sls-dynamodb/handler.js
@@ -2,7 +2,7 @@ module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless Your function executed successfully!',
       input: event,
     }, null, 2),
   };
